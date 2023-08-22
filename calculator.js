02b635ed0258c70dbbc0f3210ad1bcac0b68f68c
ogueki <shinya19970712@gmail.com>
2023-08-22 16:37:09 +0900

    ファイル分割

---
+++ calculator.js
@@ -0,0 +1,40 @@
+function appendToDisplay(value) {
+    const display = document.getElementById('display');
+    display.value += value;
+}
+
+function clearDisplay() {
+    const display = document.getElementById('display');
+    display.value = '';
+}
+
+function calculate() {
+    const display = document.getElementById('display');
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
+
+
+// 無意味なボタンのクリックイベント
+function meaninglessButton(emoji) {
+    // 音を鳴らす
+    const sound = document.getElementById('sound');
+    sound.play();
+}
+
+// ボタンの虹色ルーレットを始める
+function startRainbow() {
+    const button = document.querySelector('.meaningless-button');
+    button.style.animation = 'rainbow 1s infinite';
+}
+
+// 虹色ルーレットを止める
+function stopRainbow() {
+    const button = document.querySelector('.meaningless-button');
+    const computedStyle = window.getComputedStyle(button);
+    button.style.animation = '';
+    button.style.backgroundColor = computedStyle.backgroundColor;
+}
